c9c92b71a9abafc87ec859447be25980b868ba59
Samuel Abrokwa <tkromeo2003@gmail.com>
2026-05-23 18:04:57 +0100

    Flash Message, AppButton & AppTextInput

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppText from './src/components/texts/AppText';
 import AppSaveView from './src/components/views/AppSaveView';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
+import AppButton from './src/components/buttons/AppButton';
+import AppTextInput from './src/components/inputs/AppTextInput';
 
 export default function App() {
   return (
+    <>
+    <FlashMessage position={"top"}/>
     <AppSaveView style={styles.container}>
-      <AppText variant='medium'>Hello World!</AppText>
-      <AppText variant='bold'>Hello World!</AppText>
+      <AppTextInput 
+      placeholder='Enter Name'
+      
+      />
     </AppSaveView>
+    </>
   );
 }
 
