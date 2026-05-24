1b000395359baac0de1e32dcaeb8ce6397f50862
Samuel Abrokwa <tkromeo2003@gmail.com>
2026-05-24 01:27:49 +0100

    Sign In Screen, Sign Up Screen & Stack Navigation

--- App.tsx
+++ App.tsx
@@ -5,17 +5,18 @@ import AppSaveView from './src/components/views/AppSaveView';
 import FlashMessage, { showMessage } from 'react-native-flash-message';
 import AppButton from './src/components/buttons/AppButton';
 import AppTextInput from './src/components/inputs/AppTextInput';
+import SignInScreen from './src/screens/auth/SignInScreen';
+import SignUpScreen from './src/screens/auth/SignUpScreen';
+import AuthStack from './src/navigation/AuthStack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <>
-    <FlashMessage position={"top"}/>
-    <AppSaveView style={styles.container}>
-      <AppTextInput 
-      placeholder='Enter Name'
-      
-      />
-    </AppSaveView>
+    <NavigationContainer>
+      <FlashMessage position={"top"}/>
+      <AuthStack />
+    </NavigationContainer>
     </>
   );
 }
