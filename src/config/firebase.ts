03f0638d9cde130cfbf86b499818110f79e85d7f
Samuel Abrokwa <tkromeo2003@gmail.com>
2026-06-08 21:13:40 +0100

    Setup db and implemented receipt of data from db

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,31 +3,25 @@ import { initializeApp } from 'firebase/app';
 // Optionally import the services that you want to use
 import {getAuth} from 'firebase/auth';
 // import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
+import {getFirestore} from 'firebase/firestore';
 // import {...} from 'firebase/functions';
 // import {...} from 'firebase/storage';
 
 // Initialize Firebase
 
-// MaskedInitialize Firebase
-
+//Initialize Firebase
 
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+  
 };
+
 
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+const db = getFirestore(app)
 
-export {auth};
+export {auth, db};
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,17 +1,28 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, {useState, useEffect } from "react";
 import AppSaveView from "../../components/views/AppSaveView";
 import HomeHeader from "../../components/headers/HomeHeader";
 import { AppFonts } from "../../styles/fonts";
 import ProductCard from "../../components/cards/ProductCard";
-import { products } from "../../data/products";
+//import { products } from "../../data/products";
 import { s, vs } from "react-native-size-matters";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../../store/reducers/cartSlice";
+import { getProductsData } from "../../config/dataServices";
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
+  const [products, setProducts] = useState([])
 
+  const fetchData = async () => {
+    const data = await getProductsData()
+    console.log(data);
+    setProducts(data);
+  }
+
+  useEffect(()=> {
+    fetchData()
+  }, [])
 
   return (
     <AppSaveView>
